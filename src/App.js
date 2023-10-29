33946a8d3676693ff009a4f77b51f04c71cb7c09
Siddhant Prasad <siddhantprasad.tvnl@gmail.com>
2023-10-29 18:17:45 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Navs from './components/Navbar.js';
+import Home from './components/Home';
+import Header from './components/Navbar.js';
+// import { Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import FormHandler from './components/FormHandler';
+// import Home from './components/Home';
 
 function App() {
   return (
-    <>
-      <Navs/>
-    </>
+  <>
+  <Header/>
+  <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path='/form' element={<FormHandler/>} />
+  </Routes>
+  </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
+
 function Header() {
   return (
     <>
@@ -9,12 +10,14 @@ function Header() {
         <Container>
           <Navbar.Brand href="/">HacktoberFest23-ReactLib</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="/">
+              Home
+            </Nav.Link>
             <Nav.Link href="#features">
               Button
             </Nav.Link>
             <Nav.Link href="/form">
-              Form
+              Form 
             </Nav.Link>
           </Nav>
         </Container>
